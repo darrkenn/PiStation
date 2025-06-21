2a3efa3ff52a02a3ceab21b3089f96d0432ecfd4
darragh <darrkenn08@gmail.com>
2025-06-21 00:43:45 +0000

    Gets latest fake temp and humidity from sqlite database. Fake data is generated from an externally running sqlite database.

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 
+#Files
+from Database_Actions.databaseCheck import *
+from Database_Actions.getWeatherValues import getValues
+
+#Checks if sqlite database is present if not then it adds fills it.
+createDb()
+
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template("homepage.html")
+def welcome():
+    return render_template("welcome.html",)
 
+@app.route('/homepage')
+def homepage():
+    values = getValues()
+    return render_template(
+        "homepage.html",
+        temp=values[0],
+        humidity=values[1]
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
